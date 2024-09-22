4f1474b96735239b6929b811c3dab4866b974a5e
kropt <SlavaCh2018@yandex.ru>
2024-09-22 18:05:19 +0500

    1 Эхо бот

---
+++ src/main/java/Token.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Token {
+    public static String ReadToken() {
+        try {
+            Scanner scanner = new Scanner(new File("../token.txt"));
+            String name = scanner.nextLine();
+            scanner.close();
+            return name;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
