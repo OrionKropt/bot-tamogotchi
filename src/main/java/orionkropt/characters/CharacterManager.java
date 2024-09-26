9b502cd355d34e612031480e82df8f44a9dac515
kropt <SlavaCh2018@yandex.ru>
2024-09-26 11:35:00 +0500

    20 Добавленна выборка персонажа через кнопки

--- src/main/java/orionkropt/characters/CharacterManager.java
+++ src/main/java/orionkropt/characters/CharacterManager.java
@@ -2,30 +2,38 @@ package orionkropt.characters;
 
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class CharacterManager {
-    private static final Map<Long, Character> characters = new HashMap<>();
+    private static final HashMap<Long, Character> userCharacters = new HashMap<>();
+    private static final HashMap<String, Character> characters = new HashMap<>();
+    private static final HashMap<String, String> typeToSendingType = new HashMap<>();
+
+    CharacterManager() {
+        characters.put("pig_selected", new Character("pig"));
+        characters.put("other_selected", null);
+
+        typeToSendingType.put("pig", "Свинка");
+    }
 
     StatusCode addCharacter(final Long id, final Character character) {
         if (character == null) {
             System.out.println("Error: character is null");
             return StatusCode.ERROR;
         }
-        if (characters.containsKey(id)) {
+        if (userCharacters.containsKey(id)) {
             System.out.println("Error: character already exists");
             return StatusCode.ERROR;
         }
-        characters.put(id, character);
+        userCharacters.put(id, character);
         return StatusCode.SUCCESS;
     }
 
     StatusCode removeCharacter(final Long id) {
-        if (!characters.containsKey(id)) {
+        if (!userCharacters.containsKey(id)) {
             System.out.println("Error: character does not exist");
             return StatusCode.ERROR;
         }
-        characters.remove(id);
+        userCharacters.remove(id);
         return StatusCode.SUCCESS;
     }
 
@@ -34,15 +42,31 @@ public class CharacterManager {
             System.out.println("Error: character is null");
             return StatusCode.ERROR;
         }
-        if (!characters.containsKey(id)) {
+        if (!userCharacters.containsKey(id)) {
             System.out.println("Error: character does not exist");
             return StatusCode.ERROR;
         }
-        characters.put(id, character);
+        userCharacters.put(id, character);
         return StatusCode.SUCCESS;
     }
 
     Character getCharacter(final Long id) {
-        return characters.get(id);
+        return userCharacters.get(id);
+    }
+
+    Character createCharacter(final String key) {
+        if (key == null) {
+            System.out.println("Error: character key is null");
+            return null;
+        }
+        if (!characters.containsKey(key)) {
+            System.out.println("Error: character does not exist");
+            return null;
+        }
+        return characters.get(key);
+    }
+
+    String getSendingType(Character character) {
+        return typeToSendingType.get(character.getType());
     }
 }
